c463b9026d80625d633c543a00b1c6431f12e808
yencarnacion07 <yoniber.encarnacion@gmail.com>
2023-01-15 23:44:58 -0400

    href="#about"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+        <a href="#about" className=" title-font font-medium text-white mb-4 md:mb-0 ">
           <a href="#about" className="ml-3 text-xl">
             Yoniber
           </a>
